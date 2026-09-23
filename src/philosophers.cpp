bd2d26bb652b704041ff925b09425d5da70f8e57
wottar <mrwocior@gmail.com>
2026-09-23 22:49:40 +0200

    Further refactoring, added CMakeLists

--- src/philosophers.cpp
+++ src/philosophers.cpp
@@ -6,50 +6,57 @@
 #include <chrono>
 #include <random>
 #include <vector>
+#include <memory>
+#include <atomic>
+#include <csignal>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 std::mutex terminal;
-std::shared_ptr<std::mutex[]> w;
-std::shared_ptr<bool[]> ma_widelec;
-std::shared_ptr<std::condition_variable[]> cv;
+std::vector<std::unique_ptr<std::mutex>> w;
+std::vector<bool> ma_widelec;
+std::vector<std::unique_ptr<std::condition_variable>> cv;
+std::atomic<bool> running{true};
 
-void Wyswietlanie(int filozofowie) {
-    //INTERFACE OF NON-CHANGEABLE INFORMATION SUCH AS NAME OF COLLUMNS
-    WINDOW *legenda = newwin(filozofowie*3, COLS, 0, 0);
-    wattron(legenda, A_BOLD);
-    mvwprintw(legenda, 0, 0, "PHILOSOPHERS:");
-    mvwprintw(legenda, 0, COLS/3-2, "STATUS:");
-    mvwprintw(legenda, 0, 2*COLS/3-2, "FORKS:");
-    mvwprintw(legenda, filozofowie+2, 0, "LEGEND:");
-    wattroff(legenda, A_BOLD);
-    start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    wattron(legenda, COLOR_PAIR(1));
-    mvwprintw(legenda, filozofowie+3, 0, "T - THINKS");
-    wattroff(legenda, COLOR_PAIR(1));
-    start_color();
-    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-    wattron(legenda, COLOR_PAIR(2));
-    mvwprintw(legenda, filozofowie+4, 0, "W - WAITS");
-    wattroff(legenda, COLOR_PAIR(2));
-    start_color();
-    init_pair(3, COLOR_RED, COLOR_BLACK);
-    wattron(legenda, COLOR_PAIR(3));
-    mvwprintw(legenda, filozofowie+5, 0, "E - EATS");
-    wattroff(legenda, COLOR_PAIR(3));
-    wrefresh(legenda);
+void sig_handler(int) {
+    running = false;
 }
 
-void Filozof(int id, int filozofowie) {
+void Wyswietlanie(size_t filozofowie) {
+    //INTERFACE OF NON-CHANGEABLE INFORMATION SUCH AS NAME OF COLLUMNS
+    WINDOW *legenda = newwin(filozofowie * 3, COLS, 0, 0);
+    wattron(legenda, A_BOLD);
+    mvwprintw(legenda, 0, 0, "PHILOSOPHERS:");
+    mvwprintw(legenda, 0, COLS / 3 - 2, "STATUS:");
+    mvwprintw(legenda, 0, 2 * COLS / 3 - 2, "FORKS:");
+    mvwprintw(legenda, filozofowie + 2, 0, "LEGEND:");
+    wattroff(legenda, A_BOLD);
 
-    WINDOW *filozof_win = newwin(1, COLS/3, id+1, 0);
-    WINDOW *status_win = newwin(1, COLS/3, id+1, COLS/3);
-    WINDOW *widelce_win = newwin(1, COLS/3, id+1, 2*COLS/3);
+    wattron(legenda, COLOR_PAIR(1));
+    mvwprintw(legenda, filozofowie + 3, 0, "T - THINKS");
+    wattroff(legenda, COLOR_PAIR(1));
 
-    while(true){
+    wattron(legenda, COLOR_PAIR(2));
+    mvwprintw(legenda, filozofowie + 4, 0, "W - WAITS");
+    wattroff(legenda, COLOR_PAIR(2));
+
+    wattron(legenda, COLOR_PAIR(3));
+    mvwprintw(legenda, filozofowie + 5, 0, "E - EATS");
+    wattroff(legenda, COLOR_PAIR(3));
+
+    wrefresh(legenda);
+    delwin(legenda);
+}
+
+void Filozof(size_t id, size_t filozofowie) {
+
+    WINDOW *filozof_win = newwin(1, COLS / 3, id + 1, 0);
+    WINDOW *status_win  = newwin(1, COLS / 3, id + 1, COLS / 3);
+    WINDOW *widelce_win = newwin(1, COLS / 3, id + 1, 2 * COLS / 3);
+
+    while (running) {
         // PSEUDO-RANDOM NUMBERS GENERATOR
         std::random_device rd;
         std::mt19937 mt(rd()); //mersenne twister generator lcizb losowych do losowego czasu jedzenia przez filozofa
@@ -60,10 +67,8 @@ void Filozof(int id, int filozofowie) {
 
         std::unique_lock<std::mutex> terminal_lock(terminal);
 
-        mvwprintw(filozof_win, 0, 0, "Philosopher %d", id);
+        mvwprintw(filozof_win, 0, 0, "Philosopher %zu", id);
         wrefresh(filozof_win);
-        start_color();
-        init_pair(1, COLOR_GREEN, COLOR_BLACK);
         wattron(status_win, COLOR_PAIR(1));
         mvwprintw(status_win, 0, 0, "T");
         wattroff(status_win, COLOR_PAIR(1));
@@ -71,56 +76,57 @@ void Filozof(int id, int filozofowie) {
         terminal_lock.unlock();
 
         //SOLUTION FOR DEADLOCK AND STARVATION
-        int lewy_widelec_id = id;
-        int prawy_widelec_id = (id + 1) % filozofowie;
+        size_t lewy_widelec_id = id;
+        size_t prawy_widelec_id = (id + 1) % filozofowie;
 
-        if (id % 2 == 1)
-        {
+        if (id % 2 == 1) {
             std::swap(lewy_widelec_id, prawy_widelec_id);
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(1200));
+        if (!running) break;
 
         // -----TAKE LEFT FORK-----
-        std::unique_lock<std::mutex> lewy_widelec_lock(w[lewy_widelec_id]);
-        cv[lewy_widelec_id].wait(lewy_widelec_lock, [lewy_widelec_id]{return !ma_widelec[lewy_widelec_id];});
+        std::unique_lock<std::mutex> lewy_widelec_lock(*w[lewy_widelec_id]);
+        cv[lewy_widelec_id]->wait(lewy_widelec_lock, [lewy_widelec_id] { return !ma_widelec[lewy_widelec_id] || !running; });
+        if (!running) break;
         ma_widelec[lewy_widelec_id] = true;
 
         terminal_lock.lock();
-        start_color();
-        init_pair(2, COLOR_YELLOW, COLOR_BLACK);
         wattron(status_win, COLOR_PAIR(2));
         mvwprintw(status_win, 0, 0, "W");
         wattroff(status_win, COLOR_PAIR(2));
         wrefresh(status_win);
-        mvwprintw(widelce_win, 0, 0, "%d", lewy_widelec_id);
+        mvwprintw(widelce_win, 0, 0, "%zu", lewy_widelec_id);
         wrefresh(widelce_win);
         terminal_lock.unlock();
 
         // -----TAKE RIGHT FORK-----
-        std::unique_lock<std::mutex> prawy_widelec_lock(w[prawy_widelec_id]);
-        cv[prawy_widelec_id].wait(prawy_widelec_lock, [prawy_widelec_id]{return !ma_widelec[prawy_widelec_id];});
+        std::unique_lock<std::mutex> prawy_widelec_lock(*w[prawy_widelec_id]);
+        cv[prawy_widelec_id]->wait(prawy_widelec_lock, [prawy_widelec_id] { return !ma_widelec[prawy_widelec_id] || !running; });
+        if (!running) {
+            ma_widelec[lewy_widelec_id] = false;
+            lewy_widelec_lock.unlock();
+            cv[lewy_widelec_id]->notify_one();
+            break;
+        }
         ma_widelec[prawy_widelec_id] = true;
 
         terminal_lock.lock();
-        start_color();
-        init_pair(2, COLOR_YELLOW, COLOR_BLACK);
         wattron(status_win, COLOR_PAIR(2));
         mvwprintw(status_win, 0, 0, "W");
         wattroff(status_win, COLOR_PAIR(2));
         wrefresh(status_win);
-        mvwprintw(widelce_win, 0, 0, "  %d", prawy_widelec_id);
+        mvwprintw(widelce_win, 0, 0, "  %zu", prawy_widelec_id);
         wrefresh(widelce_win);
         terminal_lock.unlock();
 
         // -----EAT-----
         terminal_lock.lock();
-        start_color();
-        init_pair(3, COLOR_RED, COLOR_BLACK);
         wattron(status_win, COLOR_PAIR(3));
         mvwprintw(status_win, 0, 0, "E");
         wattroff(status_win, COLOR_PAIR(3));
         wrefresh(status_win);
-        mvwprintw(widelce_win, 0, 0, "%d %d", id, (id + 1) % filozofowie);
+        mvwprintw(widelce_win, 0, 0, "%zu %zu", id, (id + 1) % filozofowie);
         wrefresh(widelce_win);
         terminal_lock.unlock();
 
@@ -128,50 +134,66 @@ void Filozof(int id, int filozofowie) {
 
         // -----PUT OFF LEFT FORK-----
         terminal_lock.lock();
-        lewy_widelec_lock.unlock();
         ma_widelec[lewy_widelec_id] = false;
-        cv[lewy_widelec_id].notify_one();
+        lewy_widelec_lock.unlock();
+        cv[lewy_widelec_id]->notify_one();
         mvwprintw(widelce_win, 0, 0, " ");
         wrefresh(widelce_win);
         terminal_lock.unlock();
 
         // -----PUT OFF RIGHT FORK-----
         terminal_lock.lock();
-        prawy_widelec_lock.unlock();
         ma_widelec[prawy_widelec_id] = false;
-        cv[prawy_widelec_id].notify_one();
+        prawy_widelec_lock.unlock();
+        cv[prawy_widelec_id]->notify_one();
         mvwprintw(widelce_win, 0, 0, "   ");
         wrefresh(widelce_win);
         terminal_lock.unlock();
-
     }
+
+    delwin(filozof_win);
+    delwin(status_win);
+    delwin(widelce_win);
 }
 
 int main(int argc, char* argv[]) {
-    initscr();
-    int filozofowie = atoi(argv[1]);
+    if (argc < 2 || atoi(argv[1]) < 2) return 1; 
 
-    std::thread f[filozofowie];
-    ma_widelec.reset(new bool[filozofowie]);
-    w.reset(new std::mutex[filozofowie]);
-    cv.reset(new std::condition_variable[filozofowie]);
+    std::signal(SIGINT, sig_handler);
+
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+
+    size_t filozofowie = static_cast<size_t>(atoi(argv[1]));
+
+    ma_widelec.resize(filozofowie, false);
+    for (size_t i = 0; i < filozofowie; i++) 
+    {
+        w.push_back(std::make_unique<std::mutex>());
+        cv.push_back(std::make_unique<std::condition_variable>());
+    }
 
     Wyswietlanie(filozofowie);
 
-    for (int i = 0; i < filozofowie; i++)
-    {
-        ma_widelec[i] = false;
-    }
-
-    for (int i = 0; i < filozofowie; i++)
+    std::vector<std::thread> f(filozofowie);
+    
+    for (size_t i = 0; i < filozofowie; i++) 
     {
         f[i] = std::thread(Filozof, i, filozofowie);
     }
 
-    for (size_t i = 0; i < filozofowie; i++)
+    for (size_t i = 0; i < filozofowie; i++) 
     {
         f[i].join();
     }
+
     endwin();
     return 0;
 }
